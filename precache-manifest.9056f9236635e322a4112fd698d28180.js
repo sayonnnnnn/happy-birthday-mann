e406204ec5930a8e423e5a80a6cd8674bb310b8f
Sayon Sai Dutta <sayon.199303111@muj.manipal.edu>
2021-07-23 18:10:29 +0530

    Updates

---
+++ precache-manifest.9056f9236635e322a4112fd698d28180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa6b45e1a6a336e8aba0a4cd83efb40",
+    "url": "/happy-birthday-mann/index.html"
+  },
+  {
+    "revision": "07fd9f900570f26e9927",
+    "url": "/happy-birthday-mann/static/css/main.1acbc03f.chunk.css"
+  },
+  {
+    "revision": "2a8ce47eb7bfc95b1c0d",
+    "url": "/happy-birthday-mann/static/js/2.78186614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/happy-birthday-mann/static/js/2.78186614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07fd9f900570f26e9927",
+    "url": "/happy-birthday-mann/static/js/main.42c9a9d3.chunk.js"
+  },
+  {
+    "revision": "4b57ff9641412dd1301a",
+    "url": "/happy-birthday-mann/static/js/runtime-main.edd714d1.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/happy-birthday-mann/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
